b023850d846134791aa458f09f7738775bc395d2
Mechazawa <mega@ioexception.at>
2014-05-06 07:28:43 -0400

    refactor

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
 <?php
-function getDirectoryList ($directory) {
-    $results = array();
-    $handler = opendir($directory);
-    while ($file = readdir($handler)) {
-      if ($file != "." && $file != "..") {
-        $results[] = $file;
-      }
-    }
-    closedir($handler);
-    return $results;
-  }
-$d=getDirectoryList('img/');
+$img = array();
+$handler = opendir('img/');
+while ($file = readdir($handler))
+  if ($file != "." && $file != "..")
+    $img[] = $file;
+closedir($handler);
 ?><html>
 <head>
    <title>Our GNU/Lord and GNU/Savior is 100% sexy!</title>
@@ -21,25 +15,24 @@ $d=getDirectoryList('img/');
        about it.
    -->
    <style>
-  body {
-    margin: 0;
-    background-image: url("/noise.png");
-  }
+   body {
+     margin: 0;
+     background-image: url("/noise.png");
+   }
 
-  img {
-    text-align: center;
-    position: absolute;
-    margin: auto;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-image-orientation: from-image;
-  }
-
-    </style>
+   img {
+     text-align: center;
+     position: absolute;
+     margin: auto;
+     top: 0;
+     right: 0;
+     bottom: 0;
+     left: 0;
+     image-orientation: from-image;
+   }
+   </style>
 </head>
 <body>
-   <a href="/"><img class="stallman" src="/img/<?=$d[array_rand($d)];?>" height="100%"></a>
+   <a href="/"><img class="stallman" src="/img/<?=$img[array_rand($img)];?>" height="100%"></a>
 </body>
 </html>
